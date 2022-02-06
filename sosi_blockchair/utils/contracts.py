97a5552485abcb54decb62283bdf66e5511b83ec
ronny <ronny.coding@gmail.com>
2022-02-06 18:53:31 +1100

    FEAT: contract names

--- sosi_blockchair/utils/contracts.py
+++ sosi_blockchair/utils/contracts.py
@@ -8,3 +8,12 @@ token_contracts = {
     "":{"name": "ETH", "decimals":18},
 }
 token_contracts_by_name = {val.get("name"): {"address": key, **val} for key, val in token_contracts.items()}
+
+contract_names = {
+    "0x7a250d5630b4cf539739df2c5dacb4c659f2488d": "Uniswap V2: Router 2",
+    "0x61122b41600c59ef4248ff9818fbf0a1b43abe17": "Uniswap V2: CRBN 3", # uniswap contract for exchanging carbon CRBN tokens
+    "0x4d5ebb22982ffeccb7b3e42a624555cb313285f0": "Uniswap V2: HGET-USDT 2", # uniswap contract for swapping HGET and USDT
+    "0x0d4a11d5eeaac28ec3f61d100daf4d40471f1852": "Uniswap V2: USDT", # Uniswap contract for swapping ETH with USDT
+    "0xa0c68c638235ee32657e8f720a23cec1bfc77c77": "Polygon (Matic): Bridge", # For bridging between ethereum and polygon networks
+    "0x40ec5b33f54e0e8a33a975908c5ba1c14e5bbbdf": "Polygon (Matic): ERC20 Bridge", # For bridging ERC20 tokens between ethereum and polygon networks
+}
